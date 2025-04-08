6519b67310e1e65bc60e94532c755c6345ed99ef
Rosiany-SB <rosiannysalles@gmail.com>
2025-04-08 17:52:12 +0000

    Código da carta A01 concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,12 +3,18 @@
 int main() {
     //carta 1
     char estado = 'c';
-    char odigoDaCarta[20] = "A01";
+    char codigoDaCarta[20] = "A01";
     char nomeDaCidade[50] = "Fortaleza";
-    int população = 2000000;
+    int populacao = 2000000;
     float area = 10000000;
     float pib = 30000000;
     int pontosTuristicos = 50;
 
-    
+    printf("Digite o nome do Estado: \n");
+    scanf(" %c", &estado);
+
+    printf("Digite o código da Carta: \n");
+    scanf(" %s", codigoDaCarta);
+
+
 }
