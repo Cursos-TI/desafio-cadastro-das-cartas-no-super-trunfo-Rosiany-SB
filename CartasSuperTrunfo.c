87172c5bf51c2dbbfd72df0f6824b1b2f8869ecd
Rosiany-SB <rosiannysalles@gmail.com>
2025-04-08 22:25:28 +0000

    Código editado e corrigido Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,16 +2,16 @@
 
 int main() {
     //carta 1
-    char estado [3]=;
-    char codigoDaCarta[19];
-    char nomeDaCidade[50];
-    int populacao;
-    float area;
-    float pib;
-    int pontosTuristicos;
-    
+    char estado [10]= "c";
+    char codigoDaCarta[19] = "A01";
+    char nomeDaCidade[50] = "Fortaleza";
+    int populacao = 2000;
+    float area = 10.000;
+    float pib = 30.000;
+    int pontosTuristicos = 50;
+
     printf("Digite o nome do Estado: \n");
-    scanf("%s", &estado);
+    scanf("%9s", &estado);
 
     printf("Digite o código da Carta: \n");
     scanf("%18s", &codigoDaCarta);
@@ -31,4 +31,14 @@ int main() {
     printf("Digite pontos turísticos: \n");
     scanf("%d", &pontosTuristicos);
 
+    printf("\n--- DADOS CADASTRADOS ---\n");
+    printf("Estado: %s\n", estado);
+    printf("Código da Carta: %s\n", codigoDaCarta);
+    printf("Nome da Cidade: %s\n", nomeDaCidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.3f\n", area);
+    printf("PIB: %.3f\n", pib);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos);
+
+    return 0;
 }
